118a878e02a32d2d5743de4a9864d692a4347e32
Zeddini Iheb <80251420+iheb147@users.noreply.github.com>
2026-05-08 04:28:54 -1200

    fix: Auto-correction via PoweredCodeReview

--- src/main/java/service/IServiceAvisJoueur.java
+++ src/main/java/service/IServiceAvisJoueur.java
@@ -7,15 +7,15 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public interface IServiceAvisJoueur<AvisJoueur> {
+public interface IServiceAvisJoueur {
 
-    void ajouter(AvisJoueur av, User us) throws SQLException ;
+    void ajouter(AvisJoueur av, User us) throws SQLException;
 
-    public void modifier(int id, AvisJoueur avisModifie,int idr) throws SQLException ;
+    void modifier(int id, AvisJoueur avisModifie, int idr) throws SQLException;
 
-    void supprimer(int id) throws SQLException ;
+    void supprimer(int id) throws SQLException;
 
-    List<entities.AvisJoueur> recuperer() throws SQLException;
+    List<AvisJoueur> recuperer() throws SQLException;
 
     boolean existeAvis(int id) throws SQLException;
 }
